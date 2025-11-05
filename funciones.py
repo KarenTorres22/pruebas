3349079454dd7ea9c489acd1eb5f5ee06cbd769f
Nicolas-Al-Tel <241311534+Nicolas-Al-Tel@users.noreply.github.com>
2025-11-05 15:52:22 +0000

    Guardando cambios

--- funciones.py
+++ funciones.py
@@ -4,3 +4,13 @@ def palindromos(s):
 def reverso(n):
     n=str(n)
     return int(n[::-1])
+
+def es_primo(n):
+    cont = 0
+    for i in range(1,n):
+        if n%i == 0:
+            cont += 1
+    if cont > 2:
+        return False
+    else:
+        return True
